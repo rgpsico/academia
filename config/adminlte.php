04e81c1490e5795fc731d717d7c9d5a93e56e356
rgpsico <gerro121@hotmail.com>
2022-04-09 09:59:36 -0300

    teste

--- config/adminlte.php
+++ config/adminlte.php
@@ -45,7 +45,7 @@ return [
     |
     */
 
-    'logo' => '<b>Assscademia Apocalipse</b>',
+    'logo' => '<b>Academia Apocalipse</b>',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
