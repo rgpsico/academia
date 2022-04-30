f7c6cc1b45f3d990120e6f5f437552db7509f99f
rgpsico <gerro121@hotmail.com>
2022-04-30 06:29:42 -0300

    api

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,7 +9,7 @@ import Devedores from './components/Devedores.vue'
 
 Vue.prototype.$url_api = 'https://sistem.academiaextremeapocalipse.com.br/api/'
 Vue.prototype.$url_base = 'https://sistem.academiaextremeapocalipse.com.br/painel/'
-Vue.prototype.$url_image = 'https://sistem.academiaextremeapocalipse.com.br/painel/storage/'
+Vue.prototype.$url_image = 'https://sistem.academiaextremeapocalipse.com.br/storage/'
 
 
 const app = new Vue({
