29c286f39336bb5bfd6bee100ac6e6ef9cffcfc8
roger <rogernevesn@gmail.com>
2022-10-23 23:19:32 -0300

    alunos model agenda

--- app/Models/Alunos.php
+++ app/Models/Alunos.php
@@ -1,14 +1,24 @@
-<?php 
+<?php
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model; 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Alunos extends Model 
-{ 
+class Alunos extends Model
+{
 
-    //protected $table ='tabela';
-    // protected $fillable = ['name','title']; 
-    protected $guarded = ['id']; 
+        use HasFactory, SoftDeletes;
+        protected $fillable = ['nome', 'whatssap', 'status', 'instagran', 'avatar'];
+        public $timestamps = false;
 
+
+        public function pagamento()
+        {
+            return $this->hasMany(Pagamento::class, 'aluno_id');
+        }
 }
+
+
+
