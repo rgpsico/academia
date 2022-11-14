195e7e8ef5f50afc6d29e54088a129d0c306b545
roger <rogernevesn@gmail.com>
2022-11-14 01:14:25 -0300

    ajuste

--- resources/views/Admin/alunos/profile.blade.php
+++ resources/views/Admin/alunos/profile.blade.php
@@ -16,12 +16,12 @@
         </div>
         <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-                <li><button class='btn btn-success btPagamento'>Enviar Pagamento {{$alunos->avatar}}</button></li>
+                <li><button class='btn btn-success btPagamento'>Enviar Pagamento </button></li>
             </ol>
         </div>
     </div>
     <div class="row">
-        <div class="col-3">
+        <div class="col-12 col-md-3 col-sm-12">
             <div class="card card-primary card-outline">
                 <div class="card-body box-profile">
                     <div class="text-center">
@@ -61,16 +61,19 @@
         <div class="col-md-9">
             <div class="card">
                 <div class="card-header p-2">
+                    <h1>Series</h1>
                     <ul class="nav nav-pills">
-                        <li class="nav-item"><a class="nav-link active" href="#activity" data-toggle="tab">Progresso</a></li>
-                        {{-- <li class="nav-item"><a class="nav-link" href="#timeline" data-toggle="tab">Timeline</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#settings" data-toggle="tab">Settings</a></li> --}}
+                        <li class="nav-item"><a class="nav-link active" href="#activity" data-toggle="tab">Segunda</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#timeline" data-toggle="tab">Terça</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#settings" data-toggle="tab">Quarta</a></li>
+                        <li class="nav-item"><a class="nav-link"  data-toggle="tab">Quinta</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#timeline" data-toggle="tab">Sexta</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#settings" data-toggle="tab">Sabado</a></li>
                     </ul>
                 </div>
                 <div class="card-body">
                     <div class="tab-content">
                         <div class="tab-pane active" id="activity">
-
                             <div class="post">
                                 <div class="user-block">
                                     <img class="img-circle img-bordered-sm" src="../../dist/img/user1-128x128.jpg"
@@ -109,147 +112,6 @@
 
 
                         </div>
-
-                        <div class="tab-pane" id="timeline">
-
-                            <div class="timeline timeline-inverse">
-
-                                <div class="time-label">
-                                    <span class="bg-danger">
-                                        10 Feb. 2014
-                                    </span>
-                                </div>
-
-
-                                <div>
-                                    <i class="fas fa-envelope bg-primary"></i>
-                                    <div class="timeline-item">
-                                        <span class="time"><i class="far fa-clock"></i> 12:05</span>
-                                        <h3 class="timeline-header"><a href="#">Support Team</a> sent you an email
-                                        </h3>
-                                        <div class="timeline-body">
-                                            Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles,
-                                            weebly ning heekya handango imeem plugg dopplr jibjab, movity
-                                            jajah plickers sifteo edmodo ifttt zimbra. Babblely odeo kaboodle
-                                            quora plaxo ideeli hulu weebly balihoo...
-                                        </div>
-                                        <div class="timeline-footer">
-                                            <a href="#" class="btn btn-primary btn-sm">Read more</a>
-                                            <a href="#" class="btn btn-danger btn-sm">Delete</a>
-                                        </div>
-                                    </div>
-                                </div>
-
-
-                                <div>
-                                    <i class="fas fa-user bg-info"></i>
-                                    <div class="timeline-item">
-                                        <span class="time"><i class="far fa-clock"></i> 5 mins ago</span>
-                                        <h3 class="timeline-header border-0"><a href="#">Sarah Young</a> accepted
-                                            your friend request
-                                        </h3>
-                                    </div>
-                                </div>
-
-
-                                <div>
-                                    <i class="fas fa-comments bg-warning"></i>
-                                    <div class="timeline-item">
-                                        <span class="time"><i class="far fa-clock"></i> 27 mins ago</span>
-                                        <h3 class="timeline-header"><a href="#">Jay White</a> commented on your post
-                                        </h3>
-                                        <div class="timeline-body">
-                                            Take me to your leader!
-                                            Switzerland is small and neutral!
-                                            We are more like Germany, ambitious and misunderstood!
-                                        </div>
-                                        <div class="timeline-footer">
-                                            <a href="#" class="btn btn-warning btn-flat btn-sm">View comment</a>
-                                        </div>
-                                    </div>
-                                </div>
-
-
-                                <div class="time-label">
-                                    <span class="bg-success">
-                                        3 Jan. 2014
-                                    </span>
-                                </div>
-
-
-                                <div>
-                                    <i class="fas fa-camera bg-purple"></i>
-                                    <div class="timeline-item">
-                                        <span class="time"><i class="far fa-clock"></i> 2 days ago</span>
-                                        <h3 class="timeline-header"><a href="#">Mina Lee</a> uploaded new photos
-                                        </h3>
-                                        <div class="timeline-body">
-                                            <img src="https://placehold.it/150x100" alt="...">
-                                            <img src="https://placehold.it/150x100" alt="...">
-                                            <img src="https://placehold.it/150x100" alt="...">
-                                            <img src="https://placehold.it/150x100" alt="...">
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div>
-                                    <i class="far fa-clock bg-gray"></i>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="tab-pane" id="settings">
-                            <form class="form-horizontal">
-                                <div class="form-group row">
-                                    <label for="inputName" class="col-sm-2 col-form-label">Name</label>
-                                    <div class="col-sm-10">
-                                        <input type="email" class="form-control" id="inputName" placeholder="Name">
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
-                                    <div class="col-sm-10">
-                                        <input type="email" class="form-control" id="inputEmail" placeholder="Email">
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <label for="inputName2" class="col-sm-2 col-form-label">Name</label>
-                                    <div class="col-sm-10">
-                                        <input type="text" class="form-control" id="inputName2" placeholder="Name">
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <label for="inputExperience" class="col-sm-2 col-form-label">Experience</label>
-                                    <div class="col-sm-10">
-                                        <textarea class="form-control" id="inputExperience" placeholder="Experience"></textarea>
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <label for="inputSkills" class="col-sm-2 col-form-label">Skills</label>
-                                    <div class="col-sm-10">
-                                        <input type="text" class="form-control" id="inputSkills"
-                                            placeholder="Skills">
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <div class="offset-sm-2 col-sm-10">
-                                        <div class="checkbox">
-                                            <label>
-                                                <input type="checkbox"> I agree to the <a href="#">terms and
-                                                    conditions</a>
-                                            </label>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <div class="offset-sm-2 col-sm-10">
-                                        <button type="submit" class="btn btn-danger">Submit</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-
-
 
                     </div>
 
@@ -299,6 +161,7 @@
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script>
         $('.btPagamento').click(function(){
             $('.modal').modal('show')
@@ -334,11 +197,15 @@
                 }
             }).then(function (response) {
                 if(response.status == 200){
-                    window.location.reload();
-                } else {
-                    alert(response.message)
-                }
-            });
+                    swal("Bom Trabalho!", "Avatar enviado com sucesso", "success");
+                    setTimeout(() => {
+                        window.location.reload(); 
+                    }, 2000);
+                    
+                } 
+            }).catch(function (error) {              
+                swal("Ops!", 'o formato da imagem é inválido', "danger");
+            });;
         });
         $('#aluno_id').val({{$alunos->id}})
 
