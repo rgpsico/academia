31adbb1f6c361a4b35da122d28bf6efda1e74ab4
roger <rogernevesn@gmail.com>
2022-11-14 01:34:04 -0300

    ajuste

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,6 +1,6 @@
-<select name="{{$name}}"  id="{{$id}}" class="{{$class}} {{'w-'.$width ?? ''}}" style="{{$style}}" >
+{{-- <select name="{{$name}}"  id="{{$id}}" class="{{$class}} {{'w-'.$width ?? ''}}" style="{{$style}}" >
     <option value="" selected>Selecione</option>
     @foreach ($alunos as $aluno)
         <option value=" {{ $aluno->id }}"> {{ $aluno->nome }}</option>
     @endforeach
-</select>
+</select> --}}
