91c30f0d97f6902b710db8c4713704870ed61d31
roger <rogernevesn@gmail.com>
2022-11-14 01:20:08 -0300

    ajuste

--- resources/views/Admin/alunos/profile.blade.php
+++ resources/views/Admin/alunos/profile.blade.php
@@ -79,7 +79,7 @@
                                     <img class="img-circle img-bordered-sm" src="../../dist/img/user1-128x128.jpg"
                                         alt="user image">
                                     <span class="username">
-                                        <a href="#">Jonathan Burke Jr.</a>
+                                        <a href="#">Jonathan Burke Jr .</a>
                                         <a href="#" class="float-right btn-tool"><i class="fas fa-times"></i></a>
                                     </span>
                                     <span class="description">Shared publicly - 7:30 PM today</span>
